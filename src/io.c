05283ccf20a4a406a1b93156c663bfd1a3a69959
Antonin <jaimelesfraisesbleues@gmail.com>
2023-02-24 22:19:22 +0100

    add getline for windows

--- src/io.c
+++ src/io.c
@@ -1,14 +1,60 @@
 #include "include/io.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h> 
+
+#ifdef _WIN32
+size_t getline(char **lineptr, size_t *n, FILE *stream) 
+{
+    char *bufptr = NULL;
+    char *p = bufptr;
+    size_t size;
+    int c;
+
+    if (lineptr == NULL || stream == NULL || n == NULL) return -1;
+
+    bufptr = *lineptr;
+    size = *n;
+    c = fgetc(stream);
+
+    if (c == EOF)  return -1;
+
+    if (bufptr == NULL) 
+    {
+        bufptr = malloc(128);
+        if (bufptr == NULL) return -1;
+        size = 128;
+    }
+
+    p = bufptr;
+    while(c != EOF) 
+    {
+        if ((p - bufptr) > (size - 1))
+        {
+            size = size + 128;
+            bufptr = realloc(bufptr, size);
+            if (bufptr == NULL) return -1;
+        }
+        *p++ = c;
+        if (c == '\n') break;
+        c = fgetc(stream);
+    }
+
+    *p++ = '\0';
+    *lineptr = bufptr;
+    *n = size;
+
+    return p - bufptr - 1;
+}
+#endif
 
 char* tac_read_file(const char* filename)
 {
   FILE* fp;
   char* line = NULL;
   size_t len = 0;
-  ssize_t read;
+  size_t read;
 
   fp = fopen(filename, "rb");
   if (fp == NULL)
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "include/soka.h"
+
 #include <stdio.h>
 
 int main (int argc, char *argv[])
